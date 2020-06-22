7fbd9f611976be7900a8804228db9ac0dd117780
bcthis2020 <63401133+bcthis2020@users.noreply.github.com>
2020-06-22 18:12:29 +0530

    Create deletescript.sh

---
+++ scripts/deletescript.sh
@@ -0,0 +1,14 @@
+#!/bin/bash echo "
+
+echo "Script executed from: ${PWD}"
+pm2 describe cl-frontend > /dev/null
+
+RUNNING=$?
+
+if [ "${RUNNING}" -eq 0 ]; then
+  sudo -E pm2 stop cl-frontend
+else
+  sudo pm2 list
+  echo "Script executed from: ${PWD}"
+  sudo pwd
+fi;
