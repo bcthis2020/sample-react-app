29d28c525c5def1c9646c4ebb966c6329198893c
bcthis2020 <63401133+bcthis2020@users.noreply.github.com>
2020-06-24 14:11:15 +0530

    Update deletescript.sh

--- scripts/deletescript.sh
+++ scripts/deletescript.sh
@@ -1,14 +1,10 @@
-#!/bin/bash echo "
+#!/bin/bash
 
-echo "Script executed from: ${PWD}"
-pm2 describe cl-frontend > /dev/null
-
+sudo pm2 describe cl-frontend > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -eq 0 ]; then
-  sudo -E pm2 stop cl-frontend
+	sudo -E pm2 delete cl-frontend
 else
-  sudo pm2 list
-  echo "Script executed from: ${PWD}"
-  sudo pwd
+	sudo pm2 list
 fi;
