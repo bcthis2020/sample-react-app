eb7bdcac17efd54625448fb00502bd7cda47fb2e
bcthis2020 <63401133+bcthis2020@users.noreply.github.com>
2020-06-24 14:10:36 +0530

    Update stopscript.sh

--- scripts/stopscript.sh
+++ scripts/stopscript.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Script executed from: ${PWD}"
-pm2 describe cl-frontend > /dev/null
+sudo pm2 describe cl-frontend > /dev/null
 RUNNING=$?
 
 if [ "${RUNNING}" -eq 0 ]; then
-  sudo -E pm2 stop cl-frontend
+	sudo -E pm2 stop cl-frontend
 else
-  sudo pm2 list
-  echo "Script executed from: ${PWD}"
-  sudo pwd
+	sudo pm2 list
+	echo "Script executed from: ${PWD}"
+	sudo pwd
 fi;
